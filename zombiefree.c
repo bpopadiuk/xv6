b40f1c0056b31511f60eda91081f687c7ee4ecfd
Boris Popadiuk <pboris@babbage.cs.pdx.edu>
2018-05-02 17:57:55 -0700

    add list checking to zombiefree test

--- zombiefree.c
+++ zombiefree.c
@@ -12,6 +12,7 @@ main(void) {
     int pids[NPROC_MINUS_3];
     int pid;
 
+    // fork as many processes as the system will allow
     printf(1, "forking...\n");
     while((pid = fork()) > 0) {
         pids[i] = pid;
@@ -19,19 +20,37 @@ main(void) {
         total += 1;
     }
 
+    // children sleep for two minutes while parent does its work
+    // they will be killed before this time is up.
     if(pid == 0)
-        sleep(30 * TPS);
+        sleep(120 * TPS);
 
     printf(1, "maximum reached.\n");
     printf(1, "%d processes were created.\n", total);
+    printf(1, "\nPRESS CTRL-F (Free list should = 0)\n");
     sleep(5 * TPS);
 
+
+    // Kill all child processes, sleep to allow for checks of free list and zombie list
     for(i = 0; i < NPROC_MINUS_3; i++) {
         kill(pids[i]);
         printf(1, "killing PID %d\n", pids[i]);
+        
+        // Check that the first 5 processes were added to Zombie List
+        if(i < 5) {
+            printf(1, "\nPRESS CTRL-Z ((%d, 3) should be on Zombie List)\n", pids[i]);
+            sleep(5 * TPS);
+        }
+        
         while(1) {
-            if(wait() != -1)
+            if(wait() != -1) {
+                // Check that the first 5 processes were added to Free List
+                if(i < 5) {
+                    printf(1, "\nPRESS CTRL-F (Free List should = %d)\n", (i + 1));
+                    sleep(5 * TPS);
+                }
                 break;
+            }
         }
     }
 
